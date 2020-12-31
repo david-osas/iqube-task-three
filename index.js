14dc0f98a72343ee4e7ddf38f245c2a77d545543
david-osas <azamegbeosarumense@gmail.com>
2020-12-31 15:59:44 +0100

    Add firebase and multer

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-let {firebaseConfig} = require('utils.js');
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+const admin = require("firebase-admin");
+const serviceAccount = require("./revie-firebase-admin-config.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
+app.use(express.json());
+
+app.post('/create-review', upload.any(), (req, res) => {
+  console.log(req.files);
+  console.log(req.body);
+
+  res.send('osas welcome');
+});
+
+
+app.listen(3000, () => {
+  console.log('app started on port 3000');
+})
