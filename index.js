6fb79f60e257f18432212b2ebcbe6ba55749847c
david-osas <azamegbeosarumense@gmail.com>
2021-01-01 20:47:52 +0100

    Add bucket delete functionality and optimize create review route

--- index.js
+++ index.js
@@ -19,8 +19,17 @@ const {getMediaList} = require('./middleware/media');
 app.use(express.json());
 
 app.post('/create-review', getMediaList, async (req, res) => {
-  let mediaUrls = [];
+  const date = new Date();
+  const docRef = firebaseDb.collection('reviews').doc(uuidv4());
+  await docRef.set({
+    ...req.formText,
+    helpful: parseInt(req.formText.helpful),
+    timestamp: date.toISOString(),
+    media: []
+  });
+  res.send('review has been successfully created');
 
+  let mediaUrls = [];
   for(let path of req.mediaPaths){
     let currentFile = await new Promise((resolve, reject) => {
       firebaseBucket.upload(path, {public: true}, (err, file, apiResponse) => {
@@ -38,19 +47,15 @@ app.post('/create-review', getMediaList, async (req, res) => {
     });
   }
 
+  docRef.update({media: [...mediaUrls]});
   for(let path of req.mediaPaths){
-    fs.unlinkSync(path);
+    fs.unlink(path, (err) => {
+      if(err){
+        console.log(err);
+      }
+    });
   }
 
-  const date = new Date();
-
-  const docRef = firebaseDb.collection('reviews').doc(uuidv4());
-  await docRef.set({
-    ...req.formText,
-    timestamp: date.toISOString(),
-    media: [...mediaUrls]
-  });
-  res.send('review has been successfully created');
   return;
 });
 
@@ -112,9 +117,43 @@ app.delete('/delete-review', async (req, res) => {
   if(!id){
     return res.send('No Id was passed');
   }
-  const deleteRes = await firebaseDb.collection('reviews').doc(id).delete();
+  const docRef = firebaseDb.collection('reviews').doc(id);
+  const doc = await docRef.get();
 
-  return res.send('osas');
+  if(!doc.exists){
+    return res.send('Invalid Id, no review has that Id');
+  }
+
+  const data = doc.data();
+  for(let mediaObj of data.media){
+    let file =  firebaseBucket.file(mediaObj.id);
+    try{
+      file.delete();
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+  const deleteRes = await docRef.delete();
+
+  return res.send('review has been successfully deleted');
+});
+
+app.patch('/helpful-review', async (req, res) => {
+  const id = req.body.id;
+
+  if(!id){
+    return res.send('No Id was passed');
+  }
+  const docRef = firebaseDb.collection('reviews').doc(id);
+  const doc = await docRef.get();
+
+  if(!doc.exists){
+    return res.send('Invalid Id, no review has that Id');
+  }
+  const updateRes = await docRef.update({helpful: doc.data().helpful + 1});
+
+  return res.send('review has been successfully marked helpful');
 });
 
 app.listen(3000, () => {
