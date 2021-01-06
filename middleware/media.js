d5bf4730951264267a770da75db2123c667d7d55
david-osas <azamegbeosarumense@gmail.com>
2021-01-06 17:18:57 +0100

    Add checks for create review arguments

--- middleware/media.js
+++ middleware/media.js
@@ -6,6 +6,7 @@ const form = formidable({
   uploadDir: path.join(__dirname, '../uploads'),
   keepExtensions: true,
 });
+const fieldSet = new Set(['landlords','environment','amenities','location']);
 
 async function getMediaList(req, res, next){
   let mediaPaths = [];
@@ -22,8 +23,19 @@ async function getMediaList(req, res, next){
   for(let key in files){
     mediaPaths.push(files[key].path);
   }
+  if(Object.keys(fields).length !== fieldSet.size){
+    res.send('Invalid number of arguments passed');
+    return;
+  }
+  for(let key in fields){
+    if(!fieldSet.has(key)){
+      res.send('Invalid argument key passed');
+      return;
+    }
+  }
+
   req.mediaPaths = mediaPaths;
-  req.formText = fields;
+  req.formText = {...fields, helpful: 0};
   next();
 }
 
